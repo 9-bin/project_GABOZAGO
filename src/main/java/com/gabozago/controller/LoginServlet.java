cb8a06c4fcbebc50b0536e88a641248f0f475490
9_bin <koobin97@gmail.com>
2022-10-05 15:09:29 +0900

    60번째 커밋
    로그인서블릿 get,post경로 설정, index 경로 설정

--- src/main/java/com/gabozago/controller/LoginServlet.java
+++ src/main/java/com/gabozago/controller/LoginServlet.java
@@ -19,14 +19,14 @@ public class LoginServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		String url = "/member/login.jsp";		// 현재 url
+		String url = "/PAGE/Member/login.jsp";		// 현재 url
 		
 		// 세션 설정
 		HttpSession session = request.getSession();
 		
 		// 세션 속성 유지 -> index 페이지로 이동 ( 페이지 설정 다시!!)
 		if ( session.getAttribute("loginUser") != null) {		// 로그인한 회원 정보가 세션에 잘 저장되어있는지 확인
-			url = "index.jsp";
+			url = "PAGE/Schedule/planList.jsp";
 		}
 		
 		// 페이지 이동
@@ -39,7 +39,7 @@ public class LoginServlet extends HttpServlet {
 		request.setCharacterEncoding("UTF-8");
 		response.setContentType("text/html; charset=UTF-8");
 		
-		String url = "/member/login.jsp";	// 현재 경로
+		String url = "PAGE/Member/login.jsp";	// 현재 경로
 		MemberDao mDao = MemberDao.getInstance();	// DB연동
 		
 		// 입력받은 아이디, 비밀번호
@@ -54,7 +54,7 @@ public class LoginServlet extends HttpServlet {
 		
 		if (result == 1) {
 			System.out.println("ok");
-			url = "index.jsp";		
+			url = "PAGE/Schedule/planList.jsp";		
 			
 			// 로그인 성공시 정보 넘김
 			MemberVo mVo = mDao.getMember(userid);
