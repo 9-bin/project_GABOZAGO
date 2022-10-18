e809ba747e6caa2143834b19ab23c5ada104b875
9_bin <koobin97@gmail.com>
2022-10-18 16:39:53 +0900

    122커밋
    마커 여러개일 경우 중앙값 자동 설정

--- WebContent/Script/map2.js
+++ WebContent/Script/map2.js
@@ -1,8 +1,12 @@
 function initMap() {
-    const map = new google.maps.Map(document.getElementById("map"), {
-        zoom: 8,
-        center: { lat: 36.1676050697, lng: 128.0824067374 },
-    });
+	var mapOptions = {
+      streetViewControl: false,
+      mapTypeControl: false,
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+   }
+   
+    map = new google.maps.Map(document.getElementById('map'), mapOptions);
+    
 	for(var i = 1; i<=count; i++){
 		let loca =[];
 		var Placename = document.getElementById('pn'+i).value;
@@ -12,18 +16,28 @@ function initMap() {
 		loca.push(Latitude);
 		loca.push(Longtiude);
 		locations.push(loca);
-		alert(locations);
 	}
+	
+	bounds = new google.maps.LatLngBounds();
+	
     for (var i = 0; i < locations.length; i++) {
+    		coordinates.push(new google.maps.LatLng(locations[i][1], locations[i][2]));
         var marker = new google.maps.Marker({
             map: map,
             label: locations[i][0],
             position: new google.maps.LatLng(locations[i][1], locations[i][2]),
         });
     }
+    for (var i=0; i < count; i++) {
+    	bounds.extend(coordinates[i]);
+	}
+	map.fitBounds(bounds);
     
 }
+var map;
+var markersArray=[];
 const locations = [];
 var count= parseInt(document.getElementById('count').value);
+coordinates = [];
 
 
